58bbeea6141d33c305c0625f13ade6bc60a90794
fadi <100878877+fadibenz@users.noreply.github.com>
2024-10-30 16:04:32 +0100

    Update sentiment.py

--- SEDS_LAB4/modeling/sentiment.py
+++ SEDS_LAB4/modeling/sentiment.py
@@ -1,7 +1,6 @@
 from textblob import TextBlob
 
 def extract_sentiment(text: str):
-        '''Extract sentiment using textblob.
-        Polarity is within range [-1, 1]'''
+       
         text = TextBlob(text)
         return text.sentiment.polarity
